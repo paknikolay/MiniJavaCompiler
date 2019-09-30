3678d2329b4806a3839a8d94feb7a6c9838dabe8
Iurii Skakovskii <uta123@yandex-team.ru>
2019-09-30 20:06:08 +0300

    fix

--- Lexical_analysis/run.sh
+++ Lexical_analysis/run.sh
@@ -2,7 +2,5 @@ cd source
 flex scanner.l
 mkdir -p ../target
 g++ -o ../target/lexer main.cpp lex.yy.cc MiniJavaScanner.h MiniJavaScanner.cpp
-rm lex.yy.cc
 cd ..
-mv lexer target/lexer
 cat $1 | target/lexer
