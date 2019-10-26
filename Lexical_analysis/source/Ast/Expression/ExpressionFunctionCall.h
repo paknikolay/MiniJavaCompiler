cf41666c0a4b276d07873dbd05a533a9052a60bf
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2019-10-26 15:19:21 +0300

    Bug fix

--- Lexical_analysis/source/Ast/Expression/ExpressionFunctionCall.h
+++ Lexical_analysis/source/Ast/Expression/ExpressionFunctionCall.h
@@ -12,7 +12,12 @@
 class ExpressionFunctionCall : public ExpressionBase{
 public:
     ExpressionFunctionCall(std::unique_ptr<ExpressionBase> &object,
-                           std::vector<std::unique_ptr<ExpressionBase>> &args) : object(object.release()), args(args) {}
+                           std::vector<std::unique_ptr<ExpressionBase>> &args_) : object(object.release()){
+        args.resize(args_.size());
+        for (size_t i = 0; i < args_.size(); ++i) {
+            args[i] = std::unique_ptr<ExpressionBase>(args_[i].release());
+        }
+    }
 
 private:
     std::unique_ptr<ExpressionBase> object;
