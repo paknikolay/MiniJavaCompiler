b502f4c6c8662ea58f545e50a26ed93e39382d75
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2019-10-26 20:14:39 +0300

    Goal MainClass ClassDeclaration

---
+++ Lexical_analysis/source/Ast/Goal/Goal.h
@@ -0,0 +1,24 @@
+//
+// Created by malik on 26.10.2019.
+//
+
+#ifndef MINI_JAVA_COMPILER_GOAL_H
+#define MINI_JAVA_COMPILER_GOAL_H
+
+#include <vector>
+
+#include "../MainClass/MainClass.h"
+#include "../ClassDeclaration/ClassDeclaration.h"
+
+
+class Goal {
+public:
+    Goal(const std::shared_ptr<MainClass>& main) : main_class(main) {}
+    Goal(const std::shared_ptr<MainClass>& main, const std::vector<ClassDeclaration> classes) : main_class(main),
+            class_declarations(classes) {}
+private:
+    std::shared_ptr<MainClass> main_class;
+    std::vector<ClassDeclaration> class_declarations;
+};
+
+#endif //MINI_JAVA_COMPILER_GOAL_H
